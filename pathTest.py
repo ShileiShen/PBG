b3fcc28c97502b71c9a2772608e732777ec3630e
michaelschf1 <michaelschf1@gmail.com>
2018-05-17 22:05:52 +1000

    L shape res added

--- pathTest.py
+++ pathTest.py
@@ -15,33 +15,15 @@ print('Using gdspy module version ' + gdspy.__version__)
 #%% 
 ## Parameters ##  ALL UNITS IN MICRONS
 
-filename = 'path_Test.gds'
+filename = 'path_L1.gds'
 #filepath = 'C:/A_MYKHAILO/simulations/Pattern/EBL designes/PBG/2018'
 filepath='/Users/mykhailo/Documents/UNSW/PBG'
 
-l_Zhigh=3000
-t_Zhigh=10
 
-l_Zlow=3000
-t_Zlow=242
+l_res=8100/2   #length of lambda/2 resonator at 7.3 GHz
+t_res=100 #width of the resonator
+gap_res=75 #resonator gap for Lk=0
 
-l_res=788   #length of lambda/2 resonator at 7.3 GHz
-t_res=2 #width of the resonator
-
-gap=4 #gap between the Zlow and the ground plane
-w_gnd=300  #width of the single ground plane for CPW structure
-
-number=4 #number of Zlow-Zhigh cascades from each side of the resonator
-
-w_c=4 #C finger width
-h_c=100 #C finger height
-w_sep=4 #C finger separation
-h_sep=2 #C finger vertical distance from ground plane fingers to resonator
-delta_x=0 #offset for starting point of C fingers
-
-
-c_gap=0
-c_length=0
 ## End of parameters ##
 #%%
 # ------------------------------------------------------------------ #
