712e54d3b1be830fc24d0e4e3c2c83ae6ccba558
michaelschf1 <32321209+michaelschf1@users.noreply.github.com>
2018-12-18 14:28:55 +1100

    Update MeanderL.py

--- MeanderL.py
+++ MeanderL.py
@@ -5,11 +5,11 @@
 
 from pathLibrary import PathCreatorLibrary as p
 
-filename = 'meanderL_W3um_G5um_L300um.gds'
-filepath = 'C:/Users/z5119993/OneDrive - UNSW/research/Anderson'  # for Windows
+p.filename = 'meanderL_W3um_G9um_L300um.gds'
+p.filepath = 'C:/Users/z5119993/OneDrive - UNSW/research/Anderson'  # for Windows
 w = 3  #width of the line
 l = 300 - w  #length of the complete line
-l_trans = 5 + 2*w  #spacing between lines
+l_trans = 9 + 2*w  #spacing between lines
 N_line_up = 10  #half number of total vertical lines
 
 for i in range(N_line_up):
